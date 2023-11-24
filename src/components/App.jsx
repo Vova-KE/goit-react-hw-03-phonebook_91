c0ce297c2cb9b15a912661d73421c7f16306cc42
Volodymyr Kozyr <vroger4@gmail.com>
2023-11-24 17:43:15 +0200

    HW 3 update
    HW 3 update

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,10 +8,10 @@ import style from './style.module.css';
 class App extends Component {
   state = {
     contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: '',
   };
@@ -19,7 +19,9 @@ class App extends Component {
   componentDidMount() {
     const existingContacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(existingContacts);
-    this.setState({ contacts: parsedContacts });
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
   }
 
   componentDidUpdate(prevProps, prevState) {
